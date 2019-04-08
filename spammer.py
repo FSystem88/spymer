b9c0cc0e6669b6c8b658c95fb692b54b23f4138a
FSystem88 <41367666+FSystem88@users.noreply.github.com>
2019-04-09 00:06:30 +0300

    Add files via upload

--- spammer.py
+++ spammer.py
@@ -1,19 +1,12 @@
 #!/usr/bin/python
 
-# - Spammer v3.1
+# - Spammer v2
 # | Description: spams a phone number by sending it a lot of sms by using Grab API
-# | Author: P4kL0nc4t & Noxturnix
-# | Date: 25/04/2018
+# | Author: P4kL0nc4t
+# | Date: 5/12/2017
 # | Disclaimer: Editing author will not make you the real coder
-# | What's New?
-# | - Fixed 403 forbidden
-# | - Use less CPU
 
 import spammer_class
 spammer = spammer_class.Spammer()
-spammer.author = "P4kL0nc4t & Noxturnix"
-try:
-    spammer.main()
-except KeyboardInterrupt:
-    print spammer_class.color.FAIL+spammer_class.color.REVERSE+"\r[!][except] KeyboardInterrupt detected! Exiting . . ."+spammer_class.color.ENDC+"\n"
-    exit()
+spammer.author = "P4kL0nc4t"
+spammer.main()
