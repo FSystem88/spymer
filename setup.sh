cee63b1e8b0b432d64086c2324a6938fc0db3c8e
FSystem88 <41367666+FSystem88@users.noreply.github.com>
2019-04-16 20:32:35 +0300

    ...

--- setup.sh
+++ setup.sh
@@ -1,4 +1,4 @@
 #! /bin/sh
 cd
-echo alias spymer='cd && python2 spymer/spammer.py'>>.bashrc
-echo "\033[37;1;42m GOOD! \033[0m"
+echo alias spymer='\cd && python2 spymer/spammer.py'\>>.bashrc
+echo -e "\033[37;1;42m GOOD! \033[0m"
