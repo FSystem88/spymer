9e1c6d5af8c42823aa89c6bced87e444662877b5
FSystem88 <41367666+FSystem88@users.noreply.github.com>
2019-04-17 09:56:41 +0300

    So need to!..

--- spammer.py
+++ spammer.py
@@ -1,12 +1,12 @@
 #!/usr/bin/python
 
-# - Spammer v1
+# - BCSpammer v1
 # | Description: spams a phone number by sending it a lot of sms by using BelkaCar API
 # | Author: FSystem88
 # | Date: 09/04/2019
 # | Disclaimer: Editing author will not make you the real coder
 
 import spammer_class
-spammer = spammer_class.Spammer()
+spammer = spammer_class.BCSpammer()
 spammer.author = "FSystem88"
 spammer.main()
