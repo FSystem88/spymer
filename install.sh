36a5a3c1db7409f07d69911fb8adb1d6e3359535
FSystem88 <41367666+FSystem88@users.noreply.github.com>
2019-06-20 02:29:54 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -1,4 +1,7 @@
 #! /bin/sh
 cd
+apt install python2
+apt install python-pip
+pip2 install requests
 echo alias spymer="'cd && python2 spymer/spammer.py'">>.bashrc
 echo "\033[37;1;42m G O O D ! \033[0m"
