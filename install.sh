41b611e4a17926239bb482a08cb36d6b5d20f3ac
FSystem88 <41367666+FSystem88@users.noreply.github.com>
2019-07-03 19:18:31 +0300

    the installation file
    update from python2 to python3

--- install.sh
+++ install.sh
@@ -1,7 +1,9 @@
 #! /bin/sh
 cd
-apt install python2
-apt install python-pip
-pip2 install requests
-echo alias spymer="'cd && python2 spymer/spammer.py'">>.bashrc
+apt install python3
+apt install python3-pip
+pip3 install requests
+echo alias spymer="'cd && python3 spymer/spammer.py'">>.bashrc
 echo "\033[37;1;42m G O O D ! \033[0m"
+cd
+python3 spymer/spammer.py
