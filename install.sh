f22ab1389ae8595364921b9d64ce860a9b8448cd
FSystem88 <41367666+FSystem88@users.noreply.github.com>
2019-07-03 19:19:03 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -6,4 +6,4 @@ pip3 install requests
 echo alias spymer="'cd && python3 spymer/spammer.py'">>.bashrc
 echo "\033[37;1;42m G O O D ! \033[0m"
 cd
-python3 spymer/spammer.py
+python3 spymer/spammer.py -h
