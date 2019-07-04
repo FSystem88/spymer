919abc79413fd895f842570d9b07448d5128bceb
FSystem88 <41367666+FSystem88@users.noreply.github.com>
2019-07-04 22:41:05 +0300

    Файл установки
    Переход на русский язык в связи с переходом на Python3

--- install.sh
+++ install.sh
@@ -4,6 +4,6 @@ apt install python3
 apt install python3-pip
 pip3 install requests
 echo alias spymer="'cd && python3 spymer/spammer.py'">>.bashrc
-echo "\033[37;1;42m G O O D ! \033[0m"
+echo "\033[37;1;42m G O O D !! \033[0m"
 cd
 python3 spymer/spammer.py -h
