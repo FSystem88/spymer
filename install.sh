2850a6386eb7fb55b96b772e62d732df3330760d
FSystem88 <41367666+FSystem88@users.noreply.github.com>
2019-08-22 00:03:27 +0000

    updated

--- install.sh
+++ install.sh
@@ -4,6 +4,7 @@ pkg install python
 pkg install python3
 pkg install python3-pip
 pip3 install requests
+pip3 install colorama
 echo alias spymer="'cd && python3 spymer/spammer.py'">>.bashrc
 echo "\033[37;1;42m G O O D !! \033[0m"
 cd
