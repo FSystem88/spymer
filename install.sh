dfe7c250553ba0b385f0da02003d8feb921f4d70
FSystem88 <41367666+FSystem88@users.noreply.github.com>
2019-08-29 21:08:59 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -5,10 +5,10 @@ apt install python3
 apt install python3-pip
 pip3 install requests
 pip3 install colorama
-echo clear>>.bashrc
-echo alias spymer-update="'clear && cd && rm -rf spymer && git clone https://github.com/FSystem88/spymer && clear && spymer -h '">>.bashrc
+echo clear>.bashrc
+echo alias spymer-update="'clear && cd && rm -rf spymer && git clone https://github.com/FSystem88/spymer && clear && spymer'">>.bashrc
 echo alias spymer="'clear && cd && python3 spymer/spammer.py'">>.bashrc
-echo "\033[37;1;42m G O O D !! \033[0m"
-cd
 clear
-python3 spymer/spammer.py -h
+cd
+python3 spymer/spammer.py
+echo "\033[37;1;42m Done. Please restart your Terminal... \033[0m"
