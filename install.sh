39ecf32749e5002dc8a60ef86328d42bbd9234ba
FSystem88 <41367666+FSystem88@users.noreply.github.com>
2020-01-16 18:04:23 +0300

    Update install.sh

--- install.sh
+++ install.sh
@@ -3,6 +3,9 @@ apt-get install python3, python3-pip
 pip3 install requests
 pip3 install colorama
 cp ~/spymer/spammer.py /data/data/com.termux/files/usr/bin/spymer
-cp ~/spymer/spammer.py /usr/bin/spymer
 chmod +x /data/data/com.termux/files/usr/bin/spymer
+dos2unix /data/data/com.termux/files/usr/bin/spymer
+cp ~/spymer/spammer.py /usr/bin/spymer
+chmod +x /usr/bin/spymer
+dos2unix /usr/bin/spymer
 spymer
