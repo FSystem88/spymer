167ba7520f7a7449ad5eb262674a757d7edfc80d
FSystem88 <41367666+FSystem88@users.noreply.github.com>
2020-01-17 17:14:48 +0300

    Update spammer.py

--- spammer.py
+++ spammer.py
@@ -36,7 +36,7 @@ https://github.com/crinny
 		_phone = args.phonenum
 
 		if _phone == "update":
-			cmd = os.system("cd && rm -rf ~/spymer && git clone https://github.com/FSystem88/spymer && sh ~/spymer/install.sh")
+			cmd = os.system("cd && rm -rf ~/spymer && git clone https://github.com/FSystem88/spymer && sh ~/spymer/install.sh && exit")
 			cmd = os.system("exit")
 
 		def showstatus(message, type='new'):
