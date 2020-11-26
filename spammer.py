58172b7401a686f422c4095fe69bf518adc9b4af
FSystem88 <41367666+FSystem88@users.noreply.github.com>
2020-11-27 01:31:41 +0300

    Update spammer.py

--- spammer.py
+++ spammer.py
@@ -768,7 +768,7 @@ def MAIN():
 			def update():
 				a=input("Вы уверены, что хотите обновить? (y/n) ")
 				if a=="y":
-					os.system("cd ~/spymer && rm -rf * && git pull && sh install.sh")
+					os.system("rm -rf ~/spymer && git clone https://github.com/FSystem88/spymer && cd ~/spymer && sh install.sh")
 					exit()
 				else:
 					print("Отменено")
