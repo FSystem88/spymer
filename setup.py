59a09ef319409b345b8788e23bdf270838ebfa55
Andrey <44263315+s0563858@users.noreply.github.com>
2021-02-12 15:14:50 +0100

    Update setup.py

--- setup.py
+++ setup.py
@@ -8,6 +8,8 @@ setup(
    author_email='andrey.borisov@outlook.de',
    packages=['tspam'],  #same as name
    entry_points={'console_scripts': [ 'tspam=tspam.start:MAIN']},
+       # function to call on $ python my.egg
+    py_modules=['tspam=tspam.start:MAIN'],
     license="MPL 2.0",
    install_requires=['asyncio', 'aiohttp', "requests", "colorama"], #external packages as dependencies
 )
