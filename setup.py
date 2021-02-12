5ef3246e50b10a28aee18db12ac93dce0297c117
Andrey Borisov <andreyborisov@Andreys-MBP.fritz.box>
2021-02-12 13:06:04 +0100

    new installation routine

---
+++ setup.py
@@ -0,0 +1,13 @@
+from setuptools import setup
+
+setup(
+   name='tspam',
+   version='0.1',
+   description='A very simple sms-spammer',
+   author='AndreyBorisov',
+   author_email='andrey.borisov@outlook.de',
+   packages=['tspam'],  #same as name
+   entry_points={'console_scripts': [ 'tspam=tspam.start:MAIN']},
+    license="MPL 2.0",
+   install_requires=['asyncio', 'aiohttp', "requests", "colorama"], #external packages as dependencies
+)
